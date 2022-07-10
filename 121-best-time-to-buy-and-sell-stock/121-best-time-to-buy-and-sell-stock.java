9d507f838bf06b15bc18be59a3d9b15c75cbb1fa
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-07-10 12:38:22 +0900

    Time: 2 ms (93.20%), Space: 84.4 MB (20.12%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,17 @@
-/*
-산 다음에만 팔 수 있음
-무조건 최댓값을 고르는게 답이 아님
-
-예제
-[7,1,3] = 2
-[5,6,1,4] = 3 (1, 4)
-
-일단 차이값 갱신하고, 작아지면 min 값 갱신하기, 커지면 차이값 갱신하기
-
-*/
-
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0];
-        int difference = 0;
         
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (min > prices[i + 1]) {
-                min = prices[i + 1];
-            }
-            if (min < prices[i + 1]) {
-                difference = Math.max(difference, prices[i + 1] - min);
+        int max = 0;
+        int min = prices[0];
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (min < prices[i]) {
+                max = Math.max(prices[i] - min, max);
+            } else {
+                min = prices[i];
             }
         }
-        return difference;
+        
+        return max;
     }
 }
