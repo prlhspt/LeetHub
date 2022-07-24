02bd5960fe0bb347df06433898fd0b568d14d5fc
Hyunseok Lim <51734550+prlhspt@users.noreply.github.com>
2022-07-24 16:01:37 +0900

    Time: 8 ms (10.48%), Space: 71.2 MB (82.41%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        
+        int result = 0;
+        
+        while (left < right) {
+            result = Math.max(result, Math.abs(right - left) * Math.min(height[left], height[right]));
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return result;
+
+    }
+}
